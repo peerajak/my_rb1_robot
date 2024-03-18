2a133473a12ddc8bc061489566d1f596b9a82957
peerajak <peerajak@gmail.com>
2024-03-18 05:48:37 +0000

    odometer used in calculation, but still robot is moving wrongly, some calculation error

--- my_rb1_ros/src/rotate_service.cpp
+++ my_rb1_ros/src/rotate_service.cpp
@@ -1,64 +1,59 @@
 #include "my_rb1_ros/Rotate.h"
+#include "nav_msgs/Odometry.h"
 #include "ros/ros.h"
 #include <cstdlib>
+#include <geometry_msgs/Quaternion.h>
 #include <geometry_msgs/Twist.h>
+#include <tf/tf.h>
+
 // Import the service message header file generated from the Empty.srv message
+double yaw_rad = 0., pitch_rad = 0., roll_rad = 0., target_yaw_rad = 0.;
 geometry_msgs::Twist ling;
 ros::Publisher pub;
 bool startstop = true;
-void setCircularMovement() {
-  ling.linear.x = 2;
-  ling.linear.y = 0;
-  ling.linear.z = 0;
-  ling.angular.x = 0;
-  ling.angular.y = 0;
-  ling.angular.z = 2;
-}
-void setStopMovement() {
-  ling.linear.x = 0;
-  ling.linear.y = 0;
-  ling.linear.z = 0;
-  ling.angular.x = 0;
-  ling.angular.y = 0;
-  ling.angular.z = 0;
-}
 
 // We define the callback function of the service
 bool my_callback(my_rb1_ros::Rotate::Request &req,
                  my_rb1_ros::Rotate::Response &res) {
 
   ROS_INFO("Request Data==> degree=%d", req.degrees);
-  if (req.degrees > 5) {
-    res.result = "success";
-    ROS_INFO("sending back response:true");
-  } else {
-    res.result = "failed";
-    ROS_INFO("sending back response:false");
-  }
-  int i = 0;
-  setCircularMovement();
-  while (i < req.degrees) {
-    pub.publish(ling);
-    usleep(1000000); // We set 1000000 because the time is set in microseconds
-    i++;
-  }
-  setStopMovement();
+  target_yaw_rad = req.degrees * 3.14 / 180;
   return true;
+}
+
+void odomCallback(const nav_msgs::Odometry::ConstPtr &msg) {
+
+  ROS_INFO("Seq: [%d]", msg->header.seq);
+  ROS_INFO("Position-> x: [%f], y: [%f], z: [%f]", msg->pose.pose.position.x,
+           msg->pose.pose.position.y, msg->pose.pose.position.z);
+  ROS_INFO("Orientation-> x: [%f], y: [%f], z: [%f], w: [%f]",
+           msg->pose.pose.orientation.x, msg->pose.pose.orientation.y,
+           msg->pose.pose.orientation.z, msg->pose.pose.orientation.w);
+  ROS_INFO("Vel-> Linear: [%f], Angular: [%f]", msg->twist.twist.linear.x,
+           msg->twist.twist.angular.z);
+
+  tf::Quaternion odom_quat(
+      msg->pose.pose.orientation.x, msg->pose.pose.orientation.y,
+      msg->pose.pose.orientation.z, msg->pose.pose.orientation.w);
+  tf::Matrix3x3 matrix_tf(odom_quat);
+  matrix_tf.getRPY(roll_rad, pitch_rad, yaw_rad);
 }
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "my_rb1_service_server");
   ros::NodeHandle nh;
   pub = nh.advertise<geometry_msgs::Twist>("cmd_vel", 1000);
+  ros::Subscriber sub = nh.subscribe("/odom", 1000, odomCallback);
   ros::ServiceServer my_service = nh.advertiseService(
       "/rotate_robot",
       my_callback); // create the Service called // my_service
                     // with the defined // callback
-
   ros::Rate loop_rate(2);
 
   while (ros::ok()) {
+    ling.angular.z = 0.5 * (target_yaw_rad - yaw_rad);
     pub.publish(ling);
+    ROS_INFO("taeget=%f current:%f", target_yaw_rad, yaw_rad);
     ros::spinOnce();
     loop_rate.sleep();
   }
